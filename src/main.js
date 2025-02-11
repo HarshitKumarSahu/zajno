832aa3d4e60eb6b34aec9d51f9b716318833ccb0
HarshitKumarSahu <poltuharshit14@gmail.com>
2025-02-11 19:34:52 +0530

    added basic threejs scene

--- src/main.js
+++ src/main.js
@@ -1,20 +1,11 @@
+import * as THREE from 'three';
 import LocomotiveScroll from 'locomotive-scroll';
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
 const locomotiveScroll = new LocomotiveScroll();
 
-// var swiper = new Swiper(".mySwiper", {
-//   slidesPerView: 4,
-//   spaceBetween: 100,
-//   freeMode: true,
-//   pagination: {
-//     clickable: false,
-//   },
-// });
-
 var swiper = new Swiper(".mySwiper", {
-  // slidesPerView: 4,
   freeMode: true,
   pagination: {
     clickable: false,
@@ -22,11 +13,82 @@ var swiper = new Swiper(".mySwiper", {
   breakpoints: {
     600: {
       slidesPerView: 4,
-      spaceBetween: 100, // For width greater than 600px
+      spaceBetween: 100,
     },
     0: {
       slidesPerView: 2,
-      spaceBetween: 15, // For width less than 600px
+      spaceBetween: 15,
     }
   }
 });
+
+// Setup
+const canvas = document.querySelector('.canvas');
+const renderer = new THREE.WebGLRenderer({ 
+  canvas,
+  antialias: true,
+  alpha : true
+ });
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  25, 
+  window.innerWidth / window.innerHeight, 
+  0.1, 
+  100
+);
+camera.position.z = 5;
+
+// Clock
+const clock = new THREE.Clock();
+
+// Geometry
+const geometry = new THREE.PlaneGeometry(1,1);
+
+const material = new THREE.MeshBasicMaterial({
+  color: 'red'
+});
+// Material
+// const material = new THREE.ShaderMaterial({
+//   vertexShader: vertex,
+//   fragmentShader: fragment,
+//   side : THREE.DoubleSide,
+//   // wireframe : true,
+//   uniforms: {
+//     uTime : { value : 0 },
+//     uColorChange : { value : 0 }
+//   }
+// });
+
+// Mesh
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Resize handler
+const handleResize = () => {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+  
+  renderer.setSize(width, height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+};
+
+window.addEventListener('resize', handleResize);
+handleResize();
+
+// Animation
+const animate = () => {
+  const elapsedTime = clock.getElapsedTime();
+  
+  // Update uniforms
+  // material.uniforms.uTime.value = elapsedTime;
+  
+  // Render
+  renderer.render(scene, camera);
+  
+  requestAnimationFrame(animate);
+};
+
+animate();
