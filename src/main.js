110f47aebef580fd80d10daa98cb7c887ff360cb
HarshitKumarSahu <poltuharshit14@gmail.com>
2025-02-11 18:28:15 +0530

    refining style

--- src/main.js
+++ src/main.js
@@ -4,11 +4,29 @@ import 'swiper/css/bundle';
 
 const locomotiveScroll = new LocomotiveScroll();
 
+// var swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 4,
+//   spaceBetween: 100,
+//   freeMode: true,
+//   pagination: {
+//     clickable: false,
+//   },
+// });
+
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 4,
-  spaceBetween: 100,
+  // slidesPerView: 4,
   freeMode: true,
   pagination: {
     clickable: false,
   },
+  breakpoints: {
+    600: {
+      slidesPerView: 4,
+      spaceBetween: 100, // For width greater than 600px
+    },
+    0: {
+      slidesPerView: 2,
+      spaceBetween: 15, // For width less than 600px
+    }
+  }
 });
