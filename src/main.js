a03a2ee21caaf8429160e4702d06c7efa4876225
HarshitKumarSahu <poltuharshit14@gmail.com>
2025-02-12 13:29:08 +0530

    set fov and image texture in plane

--- src/main.js
+++ src/main.js
@@ -23,45 +23,47 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 // Setup
+const scene = new THREE.Scene();
+const cameraDistance = 5;
+const fov = 2 * Math.atan((window.innerHeight / 2) / cameraDistance) * (180 / Math.PI);
+const camera = new THREE.PerspectiveCamera(
+  fov, 
+  window.innerWidth / window.innerHeight, 
+  0.1, 
+  100
+);
+camera.position.z = cameraDistance;
 const canvas = document.querySelector('.canvas');
 const renderer = new THREE.WebGLRenderer({ 
   canvas,
   antialias: true,
   alpha : true
- });
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  25, 
-  window.innerWidth / window.innerHeight, 
-  0.1, 
-  100
-);
-camera.position.z = 5;
-
-// Clock
-const clock = new THREE.Clock();
-
-// Geometry
-const geometry = new THREE.PlaneGeometry(1,1);
-
-const material = new THREE.MeshBasicMaterial({
-  color: 'red'
 });
-// Material
-// const material = new THREE.ShaderMaterial({
-//   vertexShader: vertex,
-//   fragmentShader: fragment,
-//   side : THREE.DoubleSide,
-//   // wireframe : true,
-//   uniforms: {
-//     uTime : { value : 0 },
-//     uColorChange : { value : 0 }
-//   }
-// });
 
-// Mesh
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const images = document.querySelectorAll("img");
+const planes =[];
+images.forEach(image => {
+  const imageBounds = image.getBoundingClientRect();
+  const texture = new THREE.TextureLoader().load(image.src);
+  const material = new THREE.MeshBasicMaterial({ map : texture });
+  const geometry = new THREE.PlaneGeometry(imageBounds.width,imageBounds.height);
+  const plane = new THREE.Mesh(geometry  , material);
+  plane.position.set(
+    imageBounds.left - window.innerWidth / 2 + imageBounds.width / 2 , 
+    -imageBounds.top + window.innerHeight / 2 - imageBounds.height / 2 ,
+    0
+  );
+  planes.push(plane)
+  scene.add(plane);
+})
+// // Geometry & Material
+// const geometry = new THREE.PlaneGeometry(100,100);
+// const material = new THREE.MeshBasicMaterial({
+//   color: 'red'
+// });
+// // Mesh
+// const mesh = new THREE.Mesh(geometry, material);
+// scene.add(mesh);
 
 // Resize handler
 const handleResize = () => {
@@ -78,6 +80,9 @@ const handleResize = () => {
 window.addEventListener('resize', handleResize);
 handleResize();
 
+// Clock
+const clock = new THREE.Clock();
+
 // Animation
 const animate = () => {
   const elapsedTime = clock.getElapsedTime();
@@ -92,3 +97,5 @@ const animate = () => {
 };
 
 animate();
+
+
