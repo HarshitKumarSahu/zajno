3c4b10a55065e2055277d6ec4251c03ae303fa5e
HarshitKumarSahu <poltuharshit14@gmail.com>
2025-02-13 11:19:37 +0530

    created basic pixileded effect using fragment shader

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -3,6 +3,12 @@ varying vec2 vUv;
 uniform sampler2D uTexture;
 
 void main() { 
-    vec4 color = texture2D(uTexture, vUv);
+    float blocks = 20.0; //block divison
+    vec2 blockUv = floor(vUv*blocks)/blocks;
+    vec2 mouse = vec2(0.5 , 0.5);
+    float distance = length(blockUv - mouse);
+    float effect = smoothstep(0.4,0.0,distance);//effect range
+    vec2 distortion = vec2(0.03) * effect;
+    vec4 color = texture2D(uTexture, vUv + distortion);
     gl_FragColor = color;
 }
